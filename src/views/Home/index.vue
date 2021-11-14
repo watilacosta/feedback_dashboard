06d8e8abb182e223b9044d2f19dff0f7f74ea104
Watila Costa <watila.freire@repassa.com.br>
2021-11-13 22:57:16 -0300

    criando ao design da home da landpage

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,5 +1,46 @@
-<template><h1 class="font-black text-brand-main">Home</h1></template>
+<template>
+  <custom-header
+    @create-account="handleAccountCreate"
+    @login="handleLogin"
+  />
+
+  <contact />
+
+  <footer class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-500">feedbacker © 2021</p>
+  </footer>
+</template>
 
 <script>
-export default {}
+import CustomHeader from './CustomHeader.vue'
+import Contact from './Contact.vue'
+
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+export default {
+  components: { CustomHeader, Contact },
+
+  setup() {
+    const router = useRouter()
+
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+
+      if(token) {
+        router.push({ name: 'Feedbacks' })
+      }
+    })
+
+    function handleLogin() {
+
+    }
+
+    function handleAccountCreate() {
+      
+    }
+
+    return { handleLogin, handleAccountCreate }
+  }
+}
 </script>
