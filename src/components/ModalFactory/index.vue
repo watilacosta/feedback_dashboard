fea5107407ac478f207efa1fcb17a718420c53dc
Watila Costa <watila.freire@repassa.com.br>
2021-11-20 15:18:26 -0300

    impedindo que o evento se propague dentro da div para o modal nao fechar ao clicar encima do mesmo.

--- src/components/ModalFactory/index.vue
+++ src/components/ModalFactory/index.vue
@@ -4,7 +4,11 @@
       @click="handleModalToggle({ status: false })"
       v-if="state.isActive"
       class="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full bg-black bg-opacity-50">
-      <div class="fixed mx-10" :class="state.width">
+      <div
+        class="fixed mx-10"
+        :class="state.width"
+        @click.stop
+      >
         <div class="flex flex-col overflow-hidden bg-white rounded-lg animate__animated animate__fadeInDown animate__faster">
           <div class="flex flex-col px-12 py-10 bg-white">
             <component :is="state.component" />
